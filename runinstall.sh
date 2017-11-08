7448fcfac45f1181c99a208806734d8c7db5051c
chencx <345057113@qq.com>
2017-11-08 16:31:09 +0800

    支持同时编译G9/G19

--- runinstall.sh
+++ runinstall.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 ROOTFS_DIR=$1
+ROOTFS_DIR_G19=$2
 
 # 安装json库(删除cgminer)
 make install
 rm -rf ${ROOTFS_DIR}/bin/cgminer
 
 # 安装多版本cgminer
-mv innominer_* ${ROOTFS_DIR}/bin/
-
+cp innominer_* ${ROOTFS_DIR}/bin/
+mv innominer_* ${ROOTFS_DIR_G19}/bin/
