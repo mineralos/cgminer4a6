384e67cc355e25614c81c53abf0e07eac04a5253
chencx <345057113@qq.com>
2017-12-11 10:36:30 +0800

    删除T1编译

--- runmake.sh
+++ runmake.sh
@@ -7,17 +7,6 @@ ROOTFS_DIR=$1
 MAKE_JOBS=$2
 ROOTFS_DIR_G19=$3
 
-# A5
-CHIP_TYPE=A5
-sed -i "s/#define CHIP_A[0-9]/#define CHIP_A5/g" miner.h
-./autogen.sh
-LDFLAGS=-L${ROOTFS_DIR}/lib \
-CFLAGS=-I${ROOTFS_DIR}/include \
-./configure --prefix=${ROOTFS_DIR} \
---enable-bitmine_${CHIP_TYPE} --without-curses --host=arm-xilinx-linux-gnueabi --build=x86_64-pc-linux-gnu # --target=arm
-make -j${MAKE_JOBS}
-cp cgminer innominer_T1
-
 # A6
 CHIP_TYPE=A6
 sed -i "s/#define CHIP_A[0-9]/#define CHIP_A6/g" miner.h
